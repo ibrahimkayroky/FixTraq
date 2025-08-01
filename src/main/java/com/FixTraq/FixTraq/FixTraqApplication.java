efac48751fc6bc47676025f3d6c45b9fcd8a208e
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-08-01 17:26:37 +0300

    main project

---
+++ src/main/java/com/FixTraq/FixTraq/FixTraqApplication.java
@@ -0,0 +1,13 @@
+package com.FixTraq.FixTraq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixTraqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixTraqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FixTraq/FixTraq/FixTraqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FixTraq.FixTraq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixTraqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
