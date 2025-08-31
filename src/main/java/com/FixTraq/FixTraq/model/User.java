618cb9ef4eaf88214a8ec2f7530a8e9b972710a8
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-08-31 20:05:27 +0300

    Create User.java

---
+++ src/main/java/com/FixTraq/FixTraq/model/User.java
@@ -0,0 +1,30 @@
+package com.FixTraq.FixTraq.model;
+
+import com.FixTraq.FixTraq.enumeration.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
