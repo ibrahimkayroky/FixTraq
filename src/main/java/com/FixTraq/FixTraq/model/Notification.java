10d2ef1973bf8a511860cf0533cb7f3bcac56b8a
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-09-17 15:58:13 +0300

    Create Notification.java

---
+++ src/main/java/com/FixTraq/FixTraq/model/Notification.java
@@ -0,0 +1,30 @@
+package com.FixTraq.FixTraq.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    private LocalDateTime createdAt;
+
+    private boolean read = false;
+
+    @ManyToOne
+    private Vehicle vehicle;
+}
