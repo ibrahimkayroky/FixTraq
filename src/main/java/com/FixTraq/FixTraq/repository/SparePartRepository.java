d774a786128ca6dc7e033853669aa0a84f6dad8f
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-09-17 15:58:25 +0300

    Create SparePartRepository.java

---
+++ src/main/java/com/FixTraq/FixTraq/repository/SparePartRepository.java
@@ -0,0 +1,7 @@
+package com.FixTraq.FixTraq.repository;
+
+import com.FixTraq.FixTraq.model.SparePart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SparePartRepository extends JpaRepository<SparePart, Long> {
+}
