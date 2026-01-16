6ff8f6e72f36b3cef1afb3545aa57c77c0510449
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2026-01-16 19:10:00 +0200

    update
    the whole program now is working but we need to do integration

--- src/main/java/com/FixTraq/FixTraq/repository/VehicleRepository.java
+++ src/main/java/com/FixTraq/FixTraq/repository/VehicleRepository.java
@@ -4,5 +4,5 @@ import com.FixTraq.FixTraq.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-    boolean existsPlateNumber(String plateNumber);
+    boolean existsByPlateNumber(String plateNumber);
 }
